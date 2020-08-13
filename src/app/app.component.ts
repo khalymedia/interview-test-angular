e5123df3fb2e515dc2af5f26d0221615cf6b3f5f
khalymedia <68514208+khalymedia@users.noreply.github.com>
2020-08-13 02:47:57 +0100

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ApiService } from './services/api.service';
+import { ThemeService, Theme } from './services/theme.service';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  theme: Observable<Theme>;
+
+  constructor(
+    private apiService: ApiService,
+    private themeService: ThemeService
+  ) {}
+
+  ngOnInit() {
+    // this.apiService.getAllCountries().subscribe((res) => console.log(res));
+    this.theme = this.themeService.mode$;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { CountryCardComponent } from './components/country-card/country-card.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    DetailComponent,
+    CountryCardComponent,
+    DropdownComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
